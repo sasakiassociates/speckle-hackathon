e87fa99bddde16e55331ff5537525ca61dc2703b
Ken Goulding <kgoulding@sasaki.com>
2022-05-11 18:59:12 -0400

    fixed structure of colorBy

--- viewer-sandbox/src/viewer.js
+++ viewer-sandbox/src/viewer.js
@@ -2,7 +2,7 @@ import {Viewer} from './hacky-sack/viewer'
 
 import {Pane} from "tweakpane";
 
-export const doThing = async ()=> {
+export const doThing = async () => {
     const container = document.querySelector('#renderer')
     if (!container) {
         throw new Error("Couldn't find #app container!")
@@ -31,14 +31,22 @@ export const doThing = async ()=> {
     pane.addInput(PARAMS, 'title')
     pane.addInput(PARAMS, 'color')
 
-// Load demo object
-    await viewer.loadObject(
-        'https://speckle.xyz/streams/00613d79b2/objects/2decc3358e013f33da7af52fef29bb1b'
-    )
+
+    await viewer.loadObject('https://speckle.xyz/streams/00613d79b2/objects/2decc3358e013f33da7af52fef29bb1b');
+    await viewer.loadObject('https://speckle.xyz/streams/00613d79b2/objects/69bfa91caf1fc4b0c82211cd92387bfb');
+    await viewer.loadObject('https://speckle.xyz/streams/4777dea055/objects/d611a3e8bf64984c50147e3f9238c925');
     console.log(viewer.getObjectsProperties())
 
     console.log('applyFilter');
-    await viewer.applyFilter({colorBy: {type:'category', property: 'speckle_type.maxValue', colors: {'Objects.Geometry.Brep': '#9021a1'}}})
+    await viewer.applyFilter({
+        colorBy: {
+            type: 'category', property: 'speckle_type', values: {
+                'Objects.Geometry.Brep': '#a14c06',
+                'Objects.Geometry.Mesh': '#51933b'
+            }
+        },
+        ghostOthers: true
+    })
 
     viewer.on('load-progress', (a) => {
         if (a.progress >= 1) {
