2739ae0b33292b7027619e2316f7c2c254758cf2
Ken Goulding <kgoulding@sasaki.com>
2022-05-11 21:20:44 -0400

    support filter by and color by area

--- viewer-sandbox/src/viewer.js
+++ viewer-sandbox/src/viewer.js
@@ -39,10 +39,23 @@ export const doThing = async () => {
     await viewer.loadObject('https://speckle.xyz/streams/00613d79b2/objects/47216939852a6a177a8ae8b924140c65');
     await viewer.loadObject('https://speckle.xyz/streams/00613d79b2/objects/eebc05e4a8bafecff0d03316bb03bd92');
     await viewer.loadObject('https://speckle.xyz/streams/00613d79b2/objects/f84216590e0ea1db8ea90513cc1236e6');
+    await viewer.loadObject('https://speckle.xyz/streams/00613d79b2/objects/3f41e7b029b2327ba4cc6b3036619d24');
     // await viewer.loadObject('https://speckle.xyz/streams/4777dea055/objects/d611a3e8bf64984c50147e3f9238c925');
     console.log(viewer.getObjectsProperties())
 
     console.log('applyFilter');
+    const setGradientColor = async () => {
+        await viewer.applyFilter({
+            colorBy: {
+                'type': 'gradient',
+                'property': 'area',
+                'minValue': 0,
+                'maxValue': 500,
+                'gradientColors': ['#000099', '#ef75ef'],
+                default: '#000000'
+            }
+        })
+    };
     const setBrepColor = async (color) => {
         await viewer.applyFilter({
             colorBy: {
@@ -50,6 +63,14 @@ export const doThing = async () => {
                     'Objects.Geometry.Brep': color,
                     'Objects.Geometry.Mesh': '#a14c06'
                 }
+            },
+            ghostOthers: true
+        })
+    };
+    const filterArea = async () => {
+        await viewer.applyFilter({
+            filterBy: {
+                'speckle_type': ['Objects.Geometry.Polyline']
             },
             ghostOthers: true
         })
@@ -97,6 +118,18 @@ export const doThing = async () => {
     });
     btnPoly.on('click', () => {
         viewer.interactions.selectObjects(v => v.userData.speckle_type === 'Objects.Geometry.Polyline')
+    });
+    const filterPoly = pane.addButton({
+        title: 'Filter Poly',
+    });
+    filterPoly.on('click', () => {
+        filterArea();
+    });
+    const colorGradient = pane.addButton({
+        title: 'Color by Area',
+    });
+    colorGradient.on('click', () => {
+        setGradientColor();
     });
     pane.on('change', (ev) => {
         if (ev.presetKey === 'color') {
