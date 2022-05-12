52fbe1e378e6f15be0ae8c8bd38b1ce6403fc41e
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-05-12 15:51:00 -0400

    added speckle viewer object

---
+++ speckle-dir-stat/src/viewer/index.js
@@ -0,0 +1,8 @@
+// POLYFILLS
+import 'core-js'
+import 'regenerator-runtime/runtime'
+
+import Viewer from './modules/Viewer'
+import Converter from './modules/converter/Converter'
+
+export { Viewer, Converter }
